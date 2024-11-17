9c5a570596b0b4a62e0139bf25cae5b0f8a6ed28
susheel <susheel.shinedezign@gmail.com>
2024-11-17 16:09:18 +0530

    invite file added

---
+++ src/Invite.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import coinIcon from "./assets/coin-icon.png";
+
+const Invite = () => {
+<>
+asdadasdasdasdfsdfSDF
+<div className="invite">
+    <div className="app-logo">
+        <img
+            src={coinIcon}
+            alt="Coin Logo"
+            className="coin"
+            style={{
+              display: "flex",
+              margin: "0 auto",
+              maxWidth: "150px",
+            }}
+          />
+    </div>
+
+</div>
+
+</>
+}
+export default Invite;
